e4509b3534fb217a75269e9e3733e34b4cf4f5eb
Trevor Gollaher <trevorgollaher@Trevors-MacBook-Air.local>
2019-09-30 17:37:10 -0400

    deploy attempt

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ export default (
     <Router history={hashHistory}>
       <Route path='/' component={App}>
         <IndexRoute component={Home}/>
-        <Route path='/projects' component={Code} />
+        <Route path='/code' component={Code} />
         <Route path='/writing' component={Writing} />
         <Route path='/contact' component={Contact} />
       </Route>
